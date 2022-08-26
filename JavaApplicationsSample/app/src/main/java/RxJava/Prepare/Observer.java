8367c2366589660c2c6d91e10e9419e13880c8d6
kaifeng225 <19666676@qq.com>
2022-08-26 16:19:27 +0800

    delete redundant code

--- JavaApplicationsSample/app/src/main/java/RxJava/Prepare/Observer.java
+++
@@ -1,5 +0,0 @@
-package RxJava.Prepare;
-
-public interface Observer<T> {
-	void notify(T event);
-}
--- JavaApplicationsSample/app/src/main/java/org/reactivestreams/Subscription.java
+++
@@ -1,7 +0,0 @@
-package org.reactivestreams;
-
-public interface Subscription {
-
-	void request(long n);
-	void cancel();
-}
