fd95880837f55f06b1183119a3c56f283fa0bf44
rzhang2 <Ray.Zhang@activenetwork.com>
2024-10-16 19:51:05 +0800

    upgrade to jdk21

--- JavaApplicationsSample/app/src/main/java/active/utils/UrlTest.java
+++ JavaApplicationsSample/app/src/main/java/active/utils/UrlTest.java
@@ -4,7 +4,7 @@ import java.io.UnsupportedEncodingException;
 import java.net.URLDecoder;
 import java.net.URLEncoder;
 
-import org.apache.commons.lang.StringUtils;
+import org.apache.commons.lang3.StringUtils;
 
 public class UrlTest {
 	public static void main(String[] args) {
